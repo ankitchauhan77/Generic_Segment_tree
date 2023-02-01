6e9c0aa7c2b1f5ee6f92a5ee8add6ac80b5434aa
ankitchauhan77 <58948908+ankitchauhan77@users.noreply.github.com>
2023-02-02 04:21:06 +0530

    Update Generic_Segment_tree.cpp

--- Generic_Segment_tree.cpp
+++ Generic_Segment_tree.cpp
@@ -1,127 +1,57 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization ("unroll-loops")
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-ll md = 1000000007;
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0);                       \
-    cout.tie(0);
-#define pb push_back
-#define g(v, i, j) get<i>(v[j])
-#define vi vector<int>
-#define vll vector<ll>
-#define srt(v) sort(v.begin(), v.end())
 #define rsrt(v) sort(v.begin(), v.end(), greater<int>())
-#define fi(i, a, b) for (int i = a; i < b; i++)
-#define fll(i, a, b) for (ll i = a; i < b; i++)
+#define srt(v) sort(v.begin(),v.end())
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define int long long
+ll md = 1000000007;
+int inf = 1e18;
 using namespace std;
-//using namespace std::chrono;
 template <typename T>
-T pw(T a, T b)
-{
-    T c = 1, m = a;
-    while (b)
-    {
-        if (b & 1)
-            c = (c * m);
-        m = (m * m), b /= 2;
-    }
-    return c;
-}
+T pw(T a, T b) {T c = 1, m = a;while(b) {if (b & 1) c=(c*m); m=(m*m); b/=2;} return c;}
 template <typename T>
-T ceel(T a, T b)
-{
-    if (a % b == 0)
-        return a / b;
-    else
-        return a / b + 1;
-}
+T ceel(T a, T b){if (a%b==0) return a/b; else return a/b + 1;}
 template <typename T>
-T my_log(T n, T b)
-{
-    T i = 1, ans = 0;
-    while (1)
-    {
-        if (i > n)
-        {
-            ans--;
-            break;
-        }
-        if (i == n)
-            break;
-        i *= b, ans++;
-    }
-    return ans;
+T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}
+ll pwmd(ll a, ll b) {ll c = 1, m = a%md;while(b) {if (b & 1) c=(c*m)%md; m=(m*m)%md; b/=2;} return c;}
+ll modinv(ll n){return pwmd(n, md - 2);}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll l, ll r){ // gives random number in [l,r]
+  return uniform_int_distribution<ll>(l, r)(rng);
 }
-template <typename T>
-T gcd(T a, T b)
-{
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) {
+  return os << '{' << p.first << ", " << p.second << '}';
 }
-template <typename T>
-T mysqt(T a)
-{
-    T ans = 1;
-    while (ans * ans <= a)
-        ans++;
-    ans--;
-    return ans;
+template <class T, class = decay_t<decltype(*begin(declval<T>()))>,
+          class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c) {
+  os << '[';
+  for (auto it = c.begin(); it != c.end(); ++it)
+    os << &", "[2 * (it == c.begin())] << *it;
+  return os << ']';
 }
-ll pwmd(ll a, ll b)
-{
-    ll c = 1, m = a;
-    while (b)
-    {
-        if (b & 1)
-            c = (c * m) % md;
-        m = (m * m) % md;
-        b /= 2;
-    }
-    return c;
-}
-ll modinv(ll n)
-{
-    return pwmd(n, md - 2);
-}
-ll inverse(ll i)
-{
-    if (i == 1)
-        return 1;
-    return (md - ((md / i) * inverse(md % i)) % md + md) % md;
-}
-bool sortbysec(const pair<ll, ll> &a,
-               const pair<ll, ll> &b)
-{
-    return (a.second < b.second);
-}
-/* auto start = high_resolution_clock::now();
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cerr << "Time taken: "
-         << duration.count()<< "ms" <<"\n";*/
-vector<char> capl = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-vector<char> sml = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-template <typename T>
-string conv(T a)
-{
-    string b;
-    T e = 2;
-    T c = my_log(a, e);
-    c++, b = '1';
-    for (T i = 0; i < c - 1; i++)
-    {
-        T k = a >> (c - 2 - i);
-        if (k & 1)
-            b += '1';
-        else
-            b += '0';
-    }
-    if (a == 0)
-        return "0";
-    return b;
-}
+//support up to 5 args
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, N, ...) N
+#define _FE_0(_CALL, ...)
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                             \
+  _NTH_ARG(dummy, ##__VA_ARGS__, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1, _FE_0)     \
+  (MACRO, ##__VA_ARGS__)
+// #ifdef LOCAL
+#define out(x) #x " = " << x << "; "
+#define dbg(...)                                                              \
+  cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << "\n"
+// #else
+// #define debug(...) 42
+// #endif
+//--------------------------------theartofwar-------------------------------------------//
 /*
 KEYNOTES:
 ------------------------------------------
@@ -287,7 +217,7 @@ signed main()
     segtree s(6);
     s.build(v);
     s.rupd(1,3,5);
-    fi(i, 0, 6) cout << s.query(i, i).q << " ";
+    for (int i = 0; i < 6; i++) cout << s.query(i, i).q << " ";
     cout << "\n";
     cout << s.query(0, 5).q;
 }
